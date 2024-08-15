6d63a7f46a787a37c423e9557d54b49160cd2997
Black77 <islepuchin@yandex.ru>
2024-08-15 22:46:43 +0300

    add methods

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,8 @@
+import java.util.Objects;
+
 public class Book {
-    private String name;
-    private Author author;
+    private final String name;
+    private final Author author;
     private int year;
 
     public Book(String name, Author author, int year) {
@@ -23,5 +25,22 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return year == book.year && name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return author.toString() + " '" + name + "' " + year;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Author chehov = new Author("Антон", "Чехов");
+        Author chekhov = new Author("Антон", "Чехов");
         Author tolstoy = new Author("Лев", "Толстой");
-        Book firstBook = new Book("Чайка", chehov, 1974);
+        Book firstBook = new Book("Чайка", chekhov, 1974);
         Book secondBook = new Book("Война и мир", tolstoy, 1998);
 
         firstBook.setYear(1995);
+
+        if (firstBook.equals(secondBook)) {
+            System.out.println(firstBook);
+        } else {
+            System.out.println(secondBook);
+        }
     }
 }
